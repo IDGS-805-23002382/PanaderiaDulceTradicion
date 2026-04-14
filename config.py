1adaada61cc613b803b236df3d682d1ccebbd60d
ADAIR7676 <20002803@alumnos.utleon.edu.mx>
2026-04-13 22:09:44 -0600

    config

---
+++ config.py
@@ -0,0 +1,18 @@
+import os 
+
+from sqlalchemy import create_engine
+
+class Config(object):
+    SECRET_KEY="ClaveSecreta"
+    SESSION_COOKIE_SECURE=False
+
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@127.0.0.1/panaderia_db4'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    
+    
+    MONGO_URI = 'mongodb://localhost:27017/panaderia_nosql_db'
+
+    
